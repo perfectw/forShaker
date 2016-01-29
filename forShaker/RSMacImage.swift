58003a56c368095a8f458807e80464f8c44e7ca5
perfectw <roman.spirichkin@gmail.com>
2016-01-29 09:08:47 +0300

    final

--- forShaker/RSMacImage.swift
+++ forShaker/RSMacImage.swift
@@ -8,19 +8,81 @@
 
 import UIKit
 
-class RSMacImage: UIViewController {
+class RSMacImage: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    @IBOutlet weak var RSImageView: UIImageView!
+    @IBOutlet weak var RSCollectionView: UICollectionView!
+    var imgRowAndID : (Int, Int!) = (-1,nil) // nil means ID of Main Big Image
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.RSCollectionView.delegate = self
+        self.RSCollectionView.dataSource = self
+        
+        // set Image
+        if let ID = self.imgRowAndID.1 {
+        self.RSImageView.image = messages[self.imgRowAndID.0].arrayImages[ID]
+        } else {
+            self.RSImageView.image = messages[self.imgRowAndID.0].image
+        }
     }
     
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    // collectionView
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return messages[self.imgRowAndID.0].arrayImages.count
     }
     
-    
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier("RSCollectionCell", forIndexPath: indexPath)
+        // set images
+        let img = UIImageView(frame: cell.bounds)
+        img.image = messages[self.imgRowAndID.0].arrayImages[indexPath.row]
+        cell.addSubview(img)
+        cell.layer.cornerRadius = (self.view.bounds.height/4 - 12)/4
+        cell.layer.borderWidth = 0
+        cell.clipsToBounds = true
+        return cell
+    }
+    func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAtIndexPath indexPath: NSIndexPath) -> CGSize {
+        return CGSizeMake((self.view.bounds.height/4 - 12)/2, (self.view.bounds.height/4 - 12)/2)
+    }
 }
 
+
+// MARK: Structure
+class Message {
+    let id, time : Int
+    let urlArrayImages : [String]
+    let urlImage, name : String
+    var arrayImages : [UIImage] = []
+    var image : UIImage! = nil
+    init(id : Int, time : Int, urlArrayImages : [String], urlImage:  String, name: String) {
+        self.id  = id; self.time = time; self.name = name;
+        self.urlArrayImages = urlArrayImages; self.urlImage = urlImage;
+    }
+}
+var messages : [Message] = []
+
+let RSHeight : CGFloat = 240
+
+
 // MARK: EXTENSIONS
+extension UIImage {
+    static func fromColor(color: UIColor) -> UIImage {
+        let rect = CGRect(x: 0, y: 0, width: 1, height: 1)
+        UIGraphicsBeginImageContext(rect.size)
+        let context = UIGraphicsGetCurrentContext()
+        CGContextSetFillColorWithColor(context, color.CGColor)
+        CGContextFillRect(context, rect)
+        let img = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        return img
+    }
+        var uncompressedPNGData: NSData      { return UIImagePNGRepresentation(self)!        }
+        var highestQualityJPEGNSData: NSData { return UIImageJPEGRepresentation(self, 1.0)!  }
+        var highQualityJPEGNSData: NSData    { return UIImageJPEGRepresentation(self, 0.75)! }
+        var mediumQualityJPEGNSData: NSData  { return UIImageJPEGRepresentation(self, 0.5)!  }
+        var lowQualityJPEGNSData: NSData     { return UIImageJPEGRepresentation(self, 0.25)! }
+        var b:NSData   { return UIImageJPEGRepresentation(self, 0.0)!  }
+}
--- forShaker/RSTableVC.swift
+++ forShaker/RSTableVC.swift
@@ -7,24 +7,184 @@
 //
 
 import UIKit
+import Starscream
+import Alamofire
+import AFNetworking
+import SwiftyJSON
 
-class RSTableVC: UITableViewController {
+
+class RSTableVC: UITableViewController, WebSocketDelegate, UICollectionViewDelegate, UICollectionViewDataSource {
 
     @IBOutlet var RSActivity: UIActivityIndicatorView!
+    let socket = WebSocket(url: NSURL(string: "ws://54.154.96.23:8082")!)
+    var imgRowAndID : (Int, Int!) = (-1,nil) // nil means ID of Main Big Image
+    var storedOffsets = [Int: CGFloat]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.tableView.delegate = self
+        self.tableView.dataSource = self
+        self.tableView.tableFooterView = UIView(frame: CGRect.zero)
+        
+        socket.delegate = self
+        socket.connect()
+        RSActivity.center = CGPoint(x: self.view.bounds.width/2, y: (self.view.bounds.height-60)/2)
+        self.view.addSubview(RSActivity)
+        RSActivity.startAnimating()
+    }
+    
+    
+    // MARK : WebSocket
+    func websocketDidConnect(socket: WebSocket) {
+        print("websocket is connected")
+    }
+    func websocketDidDisconnect(socket: WebSocket, error: NSError?) {
+        print("websocket is disconnected: \(error?.localizedDescription)")
+    }
+    func websocketDidReceiveData(socket: WebSocket, data: NSData) {
+        print("got some data: \(data.length)")
+    }
+    func websocketDidReceiveMessage(socket: WebSocket, text: String) {
+        print("got some text: \(text)")
+        if let dataFromString = text.dataUsingEncoding(NSUTF8StringEncoding, allowLossyConversion: false) {
+            let json = JSON(data: dataFromString)
+            let url = json["url"].string
+            dispatch_async(dispatch_get_main_queue()) {
+                self.HTTPRequest(url!)
+            }
+        }
+    }
+    
+    
+    // MARK: HTTP Request
+    func HTTPRequest(url: String) {
+        Alamofire.request(.GET, url).responseJSON { response in
+                if let dummyData = response.data {
+                    let dummyJSON = JSON(data: dummyData)
+                    for (_, subJSON) in dummyJSON {
+                        let newMessages = Message(id: subJSON["id"].intValue, time: subJSON["time"].intValue, urlArrayImages: (subJSON["images"].array?.filter({ $0.string != nil }).map({ $0.string! }))!, urlImage: subJSON["url"].stringValue, name: subJSON["name"].stringValue)
+                        messages.append(newMessages)
+                    }
+                } else { print("No-No-No ;(") }
+            // sort & reload
+            messages.sortInPlace({$0.time < $1.time})
+            self.tableView.reloadData()
+            self.RSActivity.removeFromSuperview()
+            // 2download
+            dispatch_async(dispatch_get_main_queue()) {
+                self.downloadingImages()
+            }
+        }
+    }
+    
+    
+    // MARK: downloading Image
+    func downloadingImages() {
+        for msg in messages {
+            //image
+            Alamofire.request(.GET, msg.urlImage).responseJSON { response in
+                if let imgData = response.data {
+                    msg.image = UIImage(data: imgData)
+                    self.tableView.reloadData()
+                }
+            }
+            //arayImages
+            for url in msg.urlArrayImages {
+            Alamofire.request(.GET, url).responseJSON { response in
+                if let imgData = response.data {
+                    msg.arrayImages.append(UIImage(data: imgData)!)
+                    self.tableView.reloadData()
+                }
+                }
+            }
+        }
+    }
+    
+    
+    // MARK: TableView
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return messages.count
+    }
+    override func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        return RSHeight
+    }
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = self.tableView.dequeueReusableCellWithIdentifier("RSTableCell") as! RSTableCell
+        if let img = messages[indexPath.row].image  { cell.RSImage.image = img }
+        cell.RSImage.layer.cornerRadius = (RSHeight-40)/4 //forRound
+        cell.RSImage.layer.borderWidth = 0
+        cell.RSImage.clipsToBounds = true
+        cell.RSNameLabel.text = messages[indexPath.row].name
+        cell.RSnLfbel.text = "\(messages[indexPath.row].urlArrayImages.count)"
+        return cell
+    }
+    
+    override func tableView(tableView: UITableView, willDisplayCell cell: UITableViewCell, forRowAtIndexPath indexPath: NSIndexPath) {
+        
+        guard let tableViewCell = cell as? RSTableCell else { return }
+        
+        tableViewCell.setCollectionViewDataSourceDelegate(self, forRow: indexPath.row)
+        tableViewCell.collectionViewOffset = storedOffsets[indexPath.row] ?? 0
+    }
+    
+    override func tableView(tableView: UITableView, didEndDisplayingCell cell: UITableViewCell, forRowAtIndexPath indexPath: NSIndexPath) {
+        
+        guard let tableViewCell = cell as? RSTableCell else { return }
+        
+        storedOffsets[indexPath.row] = tableViewCell.collectionViewOffset
+    }
+    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+            tableView.cellForRowAtIndexPath(indexPath)!.selectionStyle = UITableViewCellSelectionStyle.None
+            self.imgRowAndID = (indexPath.row, nil)
+            self.performSegueWithIdentifier("RSSegueMacImage", sender: self)
+    }
+    
+    override func prepareForSegue(segue:(UIStoryboardSegue!), sender:AnyObject!)
+    {
+        if segue.identifier == "RSSegueMacImage" {
+            (segue.destinationViewController as! RSMacImage).imgRowAndID = self.imgRowAndID
+        }
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    // collectionView
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return messages[collectionView.tag].arrayImages.count
     }
-
-
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier("RSCollectionCell", forIndexPath: indexPath)
+        let img = UIImageView(frame: cell.bounds)
+        img.image = UIImage(data: messages[collectionView.tag].arrayImages[indexPath.row].lowQualityJPEGNSData)
+        cell.addSubview(img)
+        cell.layer.cornerRadius = (RSHeight - 100 - 24 - 12)/4  //forRound
+        cell.layer.borderWidth = 0
+        cell.clipsToBounds = true
+        return cell
+    }
+    func collectionView(collectionView: UICollectionView, didSelectItemAtIndexPath indexPath: NSIndexPath) {
+        self.imgRowAndID = (collectionView.tag, indexPath.row)
+        self.performSegueWithIdentifier("RSSegueMacImage", sender: self)
+    }
 }
+
+
 
 class RSTableCell : UITableViewCell {
     
+    @IBOutlet weak var RSImage: UIImageView!
+    @IBOutlet weak var RSNameLabel: UILabel!
+    @IBOutlet weak var RSnLfbel: UILabel!
+    @IBOutlet weak var RSCollection: UICollectionView!
+    
+        // collectionView
+        func setCollectionViewDataSourceDelegate<D: protocol<UICollectionViewDataSource, UICollectionViewDelegate>>(dataSourceDelegate: D, forRow row: Int) {
+            RSCollection.delegate = dataSourceDelegate
+            RSCollection.dataSource = dataSourceDelegate
+            RSCollection.tag = row
+            RSCollection.setContentOffset(RSCollection.contentOffset, animated:false) // Stops collection view if it was scrolling.
+            RSCollection.reloadData()
+        }
+        var collectionViewOffset: CGFloat {
+            set { RSCollection.contentOffset.x = newValue  }
+            get { return RSCollection.contentOffset.x }
+        }
 }
